c018251f16b936a3b1017addaa46e704a95e275d
LCDRSRS <sakai.0820srs@gmail.com>
2024-05-23 12:47:49 +0900

    サインイン、サインアップ、ログアウトの機能が動くようになりました

--- backend_DB/main.py
+++ backend_DB/main.py
@@ -25,11 +25,11 @@ def database_connection():
 def index():
     #セッションから必要な情報を取得
     sign_in_status = session.get("sign_in_status", False)
-    #user_num = session.get("usernum", None)
-    #user_name = session.get("username", None)
-    #user_points = session.get("points", None)
+    usernum = session.get("usernum", None)
+    accountname = session.get("accountname", None)
+    
 
-    return render_template("index.html", sign_in_status = sign_in_status)#,user_num = user_num, userName=user_name, points=user_points)
+    return render_template("index.html", sign_in_status = sign_in_status, usernum = usernum, accountname = accountname)#,user_num = user_num, userName=user_name, points=user_points)
 
 
 @app.route("/sign_in",methods=["GET"])
@@ -66,14 +66,12 @@ def sign_in_():
         user = cur.fetchone()
 
         if user:
-            session["sign_in"] = True
+            session["sign_in_status"] = True
             session["usernum"] = user['usernum']
-            session['username'] = user['username']
-            session['acountname'] = user['acountname']
+            session['accountname'] = user['accountname']
 
             usernum = user['usernum']
-            username = user['username']
-            acountname = user['acountname']
+            accountname = user['accountname']
 
         else:
             flash('メールアドレスまたはパスワードが違います')
@@ -85,14 +83,94 @@ def sign_in_():
         return redirect('/sign_in') 
 
 
-    return render_template("index.html",email = email, usernum = usernum, username = username, acountname = acountname)
+    return render_template("index.html",sign_in_status=session.get('login',True),email = email, usernum = usernum, accountname = accountname)
 
 
 @app.route("/sign_out")
 def sign_out():
-    session["sign_in_status"] = False
-    
+    session['sign_in_status'] = False
+    session['usernum'] = False
+    session['accountname'] = False
+
     return redirect("/")
+
+
+@app.route("/sign_up", methods=["GET"])
+def sign_up():
+    if session.get('sign_in'):
+        flash('すでにログインしています')
+        return redirect('/')
+    return render_template("sign_up.html")
+
+@app.route("/sign_up", methods=["POST"])
+def sign_up_():
+    flash('')
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    accountname = request.form.get("accountname")    
+
+    if not email or not password or not accountname:
+        flash('情報を正しく入力してください')
+        print(email)
+        print(password)
+        print(accountname)
+        return render_template("sign_up.html")
+
+    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+
+    #まずemailがかぶっていないかのチェック
+    check_email = 0
+
+    try:
+        cnx = database_connection()
+        cur = cnx.cursor(dictionary=True)
+        sql = 'select count(*) from users where email = %s'
+        cur.execute(sql, (email,))
+        check_email = cur.fetchone()['count(*)']
+        if check_email == 1:
+            flash('このemailアドレスは使用できません')
+            return render_template('sign_up.html')
+    except Exception as e:
+        print(e)
+
+
+    #新規追加ユーザーのusernumの決定
+    check_usernum = 1
+    check_result_usernum = 0
+
+    flag = 0
+    result = 0
+
+    try:
+        while flag == 0:
+            sql = 'select count(*) from users where usernum = %s'
+            cur.execute(sql, (check_usernum,))
+            result = cur.fetchone()['count(*)']
+            if result == 1:
+                check_usernum += 1
+            else:
+                flag += 1
+        
+        resultusernum = check_usernum
+
+    except Exception as e:
+        print(e)
+
+
+    #usernumが決定したのでusersテーブルに新規追加
+    try:
+        sql = "INSERT INTO users (usernum, email, password, accountname) VALUES (%s, %s, %s, %s)"
+        cur.execute(sql, (resultusernum,email, password, accountname))
+        cnx.commit()
+
+    except Exception as e:
+        print(e)
+
+
+    return redirect("/sign_in")
+
 
 
 
