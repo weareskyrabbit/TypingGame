ce1c7a38d06b018050e9c077d03cdee5d36dccaf
LCDRSRS <sakai.0820srs@gmail.com>
2024-08-23 13:50:07 +0900

    一部調整

--- backend_DB/static/js/script.js
+++ backend_DB/static/js/script.js
@@ -28,14 +28,14 @@ Have fun :)
   const easyButton = document.getElementById('easy_btn');
   const mediumButton = document.getElementById('nomal_btn');
   const hardButton = document.getElementById('hard_btn');
-  let wordJP1_easy = ["簡単な単語1", "簡単な単語2", "簡単な単語3"];
-  let wordJP2_easy = ["かんたんなたんご1", "かんたんなたんご2", "かんたんなたんご3"];
+  let wordJP1_easy = ["簡単な単語", "簡単な単語", "簡単な単語"];
+  let wordJP2_easy = ["かんたんなたんご", "かんたんなたんご", "かんたんなたんご"];
 
-  let wordJP1_normal = ["普通の単語1", "普通の単語2", "普通の単語3"];
-  let wordJP2_normal = ["ふつうのたんご1", "ふつうのたんご2", "ふつうのたんご3"];
+  let wordJP1_normal = ["普通の単語", "普通の単語", "普通の単語"];
+  let wordJP2_normal = ["ふつうのたんご", "ふつうのたんご", "ふつうのたんご"];
 
-  let wordJP1_hard = ["難しい単語1", "難しい単語2", "難しい単語3"];
-  let wordJP2_hard = ["むずかしいたんご1", "むずかしいたんご2", "むずかしいたんご3"];
+  let wordJP1_hard = ["難しい単語", "難しい単語", "難しい単語"];
+  let wordJP2_hard = ["むずかしいたんご", "むずかしいたんご", "むずかしいたんご"];
   
   let wordRs; // ローマ字データ1
   let wordR; // ローマ字データ2
@@ -98,58 +98,40 @@ Have fun :)
     isFirst = false;
   }
 
-  // スタート処理
-  function start() {
-    view1.style.display = 'none';
-    view2.style.display = 'block';
-    startMsg.style.display = 'block';
-
-    startFlag = true;
-    sWait = true;
-    space.classList.add('active');
-
-    flagR = flags[0];
-    flagK = flags[1];
-    flagG = flags[2];
-    flagW = flags[3];
-    flagS = flags[4];
-    flags = [flagR, flagK, flagG, flagW, flagS];
-    localStorage.setItem('flags', JSON.stringify(flags));
+  
+  
+ // ゲーム開始処理
+function start(level) {
+  if (level === 'easy') {
+    wordJP1 = wordJP1_easy;
+    wordJP2 = wordJP2_easy;
+  } else if (level === 'normal') {
+    wordJP1 = wordJP1_normal;
+    wordJP2 = wordJP2_normal;
+  } else if (level === 'hard') {
+    wordJP1 = wordJP1_hard;
+    wordJP2 = wordJP2_hard;
   }
+}
 
-  // カウントダウン処理
-  function ready() {
-    startMsg.style.display = 'none';
-    const count = document.createElement('div');
-    count.id = 'countdown';
-    startMsg.after(count);
-    let readyTime = 3;
-    count.innerHTML = readyTime;
-    const readyTimer = setInterval(() => {
-      readyTime--;
-      if (readyTime == 0) {
-        clearInterval(readyTimer);
-        count.remove();
-        gameInit();
-      }
-      count.innerHTML = readyTime;
-    }, 1000);
-  }
-
-  // ゲーム開始処理
-  function setDifficulty(level) {
-    if (level === 'easy') {
-      wordJP1 = wordJP1_easy;
-      wordJP2 = wordJP2_easy;
-    } else if (level === 'normal') {
-      wordJP1 = wordJP1_normal;
-      wordJP2 = wordJP2_normal;
-    } else if (level === 'hard') {
-      wordJP1 = wordJP1_hard;
-      wordJP2 = wordJP2_hard;
+// カウントダウン処理
+function ready() {
+  startMsg.style.display = 'none';
+  const count = document.createElement('div');
+  count.id = 'countdown';
+  startMsg.after(count);
+  let readyTime = 3;
+  count.innerHTML = readyTime;
+  const readyTimer = setInterval(() => {
+    readyTime--;
+    if (readyTime == 0) {
+      clearInterval(readyTimer);
+      count.remove();
+      gameInit();
     }
-    gameInit();
-  }
+    count.innerHTML = readyTime;
+  }, 1000);
+}
 
   function gameInit() {
     count = 0;
