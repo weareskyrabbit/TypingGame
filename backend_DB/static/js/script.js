9caffdd2195f9dcfc4f48dee86c6c05f2efc55e2
Reou0520 <reou.520@icloud.com>
2024-08-26 11:21:00 +0900

    ドラゴン画像差し替え

--- backend_DB/static/js/script.js
+++ backend_DB/static/js/script.js
@@ -134,7 +134,7 @@ Have fun :)
     let messageText = 'ドラゴンがあらわれた！';
     message.textContent = messageText;
 
-    image.src = '../static/images/nomal_image.jpg'; // easyボタンに対応する画像
+    image.src = '../static/images/nomal_image.png'; // easyボタンに対応する画像
     difficultyImage.style.display = 'block'; // 画像を表示
     message.classList.remove('hidden');
 
